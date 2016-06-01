2ba493b59ed08da9ccd3b4fc85ebf4ff00210576
fkfk123 <lulupro104home01@gmail.com>
2016-06-01 21:19:14 +0800

    sa

--- app/src/main/java/com/example/luh76/myapplication/MainActivity.java
+++ app/src/main/java/com/example/luh76/myapplication/MainActivity.java
@@ -9,9 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
